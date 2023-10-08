bbf16f7a3d7898dab8777c83c394ddf8006c41b8
glsaacke <glsaacke@crimson.ua.edu>
2023-10-08 16:44:45 -0500

    10.8.23

--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
-﻿
+﻿//C:\Users\gavin\Documents\Schoolwork\CodingMaterials\Projects\221repo\PA3\mis221-pa3-glsaacke
+
+// returned hours to main to quit the games. untested
 //***Main
+int totalHours = 0;
+int wheelHours = 0;
 
 string menuInput = RunMenu();
-while(menuInput != "3"){
-    MenuLogic(menuInput);
+while(menuInput != "3" && totalHours != 6){
+    totalHours = MenuLogic(menuInput, ref wheelHours);
     menuInput = RunMenu();
+}
+
+if(totalHours == 6){
+    System.Console.WriteLine("Your credit hours are completed. Thanks for playing!");
 }
 
 //***End Main
 
-//Method with menu priming read
+//Method with menu priming & update reads
 static string RunMenu(){
     System.Console.WriteLine("Hello! Please make a menu selection below:\n1. Password game\n2. Wheel game\n3. Exit");
     string menuInput = Console.ReadLine();
@@ -17,29 +25,41 @@ static string RunMenu(){
 }
 
 //Method making menu selection
-static void MenuLogic(string x){
+static int MenuLogic(string x, ref int wheelHours){
+    int passHours = 0;
+
     if(x == "1"){
-        PasswordGame();
+        passHours = PasswordGame();
     }
     else if (x == "2"){
-        WheelGame();
+        wheelHours = WheelGame(ref wheelHours);
     }
     else{
        Error();
     }
+    int totalHours = passHours + wheelHours;
+
+    return totalHours;
 }
 
 //Method responsible for password game
-static void PasswordGame(){
+static int PasswordGame(){
+    string check = "1";
+    int passHours = 0;
 
+    while(check == "1"){
+
+    }
+    return passHours;
+    //variable.Length = number of characters in the string
+    //char goes in single quotes
 }
 
 //Method responsible for wheel game
-static void WheelGame(){
+static int WheelGame(ref int wheelHours){
     string check = "1";
-    int wheelHours = 0;
 
-    while(check == "1"){
+    while(check == "1" && wheelHours < 3){
         Console.Clear();
         System.Console.WriteLine("Welcome to the wheel game!\n\nHere are the possible outcomes:\n1. Earn 1 credit hour\n2. Earn 2 credit hours\n3. Lose 2 credit hours\n4. Lose 1 credit hour\n5. Lose all credit hours\n6. Nothing!\n\nPress any key to spin the wheel");
         Console.ReadKey();
@@ -48,17 +68,22 @@ static void WheelGame(){
         int number = rnd.Next(5);
 
         System.Console.WriteLine("\nThe wheel landed on...\n");
-        wheelHours = WheelLogic(number, wheelHours);
+        wheelHours = WheelLogic(number, ref wheelHours);
 
         System.Console.WriteLine("Wheel game credit hours: " + wheelHours + "\n");
 
         System.Console.WriteLine("Enter 1 to spin again, enter to quit");
         check = Console.ReadLine();
     }
+    if (wheelHours >= 3){
+        wheelHours = 3;
+        System.Console.WriteLine("Your Wheel credit hours are complete. You will be returned to the main menu.");
+    }
+    return wheelHours;
 }
 
 //Method for coding wheel outcomes
-static int WheelLogic(int number, int hours){
+static int WheelLogic(int number, ref int wheelHours){
     Random rnd = new Random();
     int number2 = rnd.Next(2);
 
@@ -66,33 +91,34 @@ static int WheelLogic(int number, int hours){
         case 0:
             Console.ForegroundColor = ConsoleColor.Green;
             System.Console.WriteLine("Earn 1 credit hour!");
-            hours += 1;
+            wheelHours += 1;
             break;
         case 1:
             if(number2 == 0){
                 Console.ForegroundColor = ConsoleColor.Green;
                 System.Console.WriteLine("Earn 2 credit hours!");
-                hours += 2;
+                wheelHours += 2;
             }
             else{
                 Console.ForegroundColor = ConsoleColor.Red;
                 System.Console.WriteLine("Lose 2 credit hours");
-                hours -= 2;
+                wheelHours -= 2;
             }
             break;
         case 2:
             Console.ForegroundColor = ConsoleColor.Red;
             System.Console.WriteLine("Lose 1 credit hour!");
-            hours -= 1;
+            wheelHours -= 1;
             break;
         case 3:
             Console.ForegroundColor = ConsoleColor.DarkRed;
             System.Console.WriteLine("Lose all credit hours!");
-            if(hours > 0){
-                hours = 0;
+            if(wheelHours > 0){
+                wheelHours = 0;
             }
             else{
-                hours -= 2;
+                wheelHours -= 2;
+                System.Console.WriteLine("(hours -2 since you don't have any to lose)");
             }
             break;
         case 4:
@@ -103,7 +129,7 @@ static int WheelLogic(int number, int hours){
     Console.ResetColor();
     System.Console.WriteLine("");
 
-    return hours;
+    return wheelHours;
 }
 
 //Method for error message
